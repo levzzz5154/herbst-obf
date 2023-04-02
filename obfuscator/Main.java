849530cfc395dffeabe91e5a55d8c1e84db32542
TerrificTable <omegagaming189@gmail.com>
2023-04-02 20:05:35 +0200

    Simple java file for testing obfuscation (on single classes)

---
+++ obfuscator/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    private static final String someString = "World";
+    private static final String otherString = "Hello " + someString + "!";
+
+    public static void main(String[] args) {
+        System.out.println(otherString);
+    }
+
+}
