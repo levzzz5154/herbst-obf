09fd1cd75e61f2fde18c73de4b5e756b62df8921
TerrificTable <omegagaming189@gmail.com>
2023-04-02 21:41:47 +0200

    IntelliJ project for a simple test.jar file

---
+++ test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "Test";
+
+        for (int i=0; i < 10; i++) {
+            System.out.println(a);
+        }
+
+        System.out.println();
+        System.out.println("Hello world!");
+    }
+}
