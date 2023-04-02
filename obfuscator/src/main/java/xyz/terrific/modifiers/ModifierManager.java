2a4009aacb181445ae0c4a6dd5dab564862aec7f
TerrificTable <omegagaming189@gmail.com>
2023-04-02 21:49:24 +0200

    created function to run all transformers/modifiers on a single ClassGen

--- obfuscator/src/main/java/xyz/terrific/modifiers/ModifierManager.java
+++ obfuscator/src/main/java/xyz/terrific/modifiers/ModifierManager.java
@@ -1,8 +1,11 @@
 package xyz.terrific.modifiers;
 
+import org.apache.bcel.generic.ClassGen;
+import xyz.terrific.ClassObfuscator;
 import xyz.terrific.util.JVM;
 import xyz.terrific.util.Logger;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +32,22 @@ public class ModifierManager {
 
     public ModifierManager() {
         this(false);
+    }
+
+    public static void runModifiers(ClassGen classgen) {
+        ModifierManager.getModifiers().forEach(modifier -> {
+            try {
+                if (ModifierManager.getShouldLog()) {
+                    Logger.getInstance().info(ClassObfuscator.class, "Running " + modifier.getSimpleName());
+                }
+                modifier.getConstructor(ClassGen.class, Boolean.class)
+                        .newInstance(classgen, false)
+                        .transform();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                Logger.getInstance().error("Failed to call constructor of modifier '" + modifier.getSimpleName() + "' - " + e.getMessage());
+            }
+        });
+
     }
 
     public static List<Class<? extends Modifier>> getModifiers() {
--- test/src/Main.java
+++ test/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+    private static final String x = "st";
+    private static final String a = "Te" + x;
+
     public static void main(String[] args) {
-        String a = "Test";
+        
 
         for (int i=0; i < 10; i++) {
             System.out.println(a);
