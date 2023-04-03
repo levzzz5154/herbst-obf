530a37aeefa6573215655c8492ee0e04f82c9b2b
TerrificTable <omegagaming189@gmail.com>
2023-04-03 17:19:05 +0200

    no longer support multiple jar files at once because of how the transformmanager works (probably easy to make it work for multiple files, but honestly i cant be bothered to do so)

--- obfuscator/src/main/java/xyz/terrific/Main.java
+++ obfuscator/src/main/java/xyz/terrific/Main.java
@@ -1,8 +1,7 @@
 package xyz.terrific;
 
-import xyz.terrific.transformer.TransformerManager;
-import xyz.terrific.obfuscator.ClassObfuscator;
 import xyz.terrific.obfuscator.JarObfuscator;
+import xyz.terrific.transformer.TransformerManager;
 import xyz.terrific.util.Logger;
 
 import java.io.File;
@@ -10,9 +9,10 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 
 public class Main {
+    private static final String Name    = "Herbst";
     private static final String Version = "b0.0.1";
 
-    private static TransformerManager modfierManager;
+    private static TransformerManager tManager;
 
 
     public static void main(String[] args) {
@@ -30,36 +30,34 @@ public class Main {
             System.exit(1);
         }
 
-        Logger.getInstance().info("Starting: \n\t\tVersion: %s\n\t\tArguments: %s", Version, Arrays.toString(args));
+        Logger.getInstance().raw("%s Starting: \n\t\tVersion: %s\n\t\tArguments: %s", Name, Version, Arrays.toString(args));
 
+//        Arrays.stream(args)
+//                .map(File::new)
+//                .forEach(file -> {
+//                    if (!file.exists()) {
+//                        Logger.getInstance().error(Main.class, "file '" + file.getName() + "' doesnt exist");
+//                        return;
+//                    }
+//
+//                    Logger.getInstance().info(Main.class, "Processing file: '%s'", file.getName());
+//                    new JarObfuscator(file).init();
+//                    tManager = new TransformerManager(false);
+//                    new JarObfuscator(file).obfuscate();
+//                }
+//        );
 
+        File file = new File(args[0]);
+        Logger.getInstance().info(Main.class, "Processing file: '%s'", file.getName());
+        new JarObfuscator(file).init();
+        tManager = new TransformerManager(true);
+        new JarObfuscator(file).obfuscate();
 
-        modfierManager = new TransformerManager(true);
-
-
-
-        Arrays.stream(args)
-                .map(File::new)
-                .forEach(file -> {
-                    if (!file.exists()) {
-                        Logger.getInstance().error(Main.class, "file '" + file.getName() + "' doesnt exist");
-                        return;
-                    }
-
-                    Logger.getInstance().info(Main.class, "Processing file: '%s'", file.getName());
-                    if (file.getName().endsWith(".jar")) {
-                        new JarObfuscator(file)
-                                .obfuscate();
-                    } else {
-                        new ClassObfuscator(file)
-                                .obfuscate();
-                    }
-                }
-        );
-
-        Logger.getInstance().info((Object) "Bye :)");
+        Logger.getInstance().raw("Bye :)");
         Logger.getInstance().close();
     }
 
-
+    public static String getName() {
+        return Name;
+    }
 }
