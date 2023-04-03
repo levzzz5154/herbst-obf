a1495c01c1c1dfba22e494d6f127a51c2f1ed552
TerrificTable <omegagaming189@gmail.com>
2023-04-03 17:18:08 +0200

    funtions to get all classnodes implementing or extending a class x

--- obfuscator/src/main/java/xyz/terrific/transformer/Transformer.java
+++ obfuscator/src/main/java/xyz/terrific/transformer/Transformer.java
@@ -1,11 +1,57 @@
 package xyz.terrific.transformer;
 
-public abstract class Transformer {
-    protected boolean isJarFile;
+import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.SimpleRemapper;
+import org.objectweb.asm.tree.ClassNode;
+import xyz.terrific.obfuscator.JarObfuscator;
 
-    public Transformer(Boolean isJarFile) {
-        this.isJarFile = isJarFile;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public abstract class Transformer {
+    protected Map<String, ClassNode> classMap;
+    protected List<ClassNode> classes;
+
+    public Transformer() {
+        this.classMap = JarObfuscator.getClasses();
+        this.classes = new ArrayList<>(classMap.values());
     }
 
     public abstract void transform();
+
+
+    protected Collection<? extends ClassNode> getImplementing(ClassNode owner) {
+        return classes.stream()
+                .filter(classNode -> classNode.interfaces.contains(owner.name))
+                .collect(Collectors.toList());
+    }
+
+    protected Collection<? extends ClassNode> getExtending(ClassNode owner) {
+        List<ClassNode> extending = new ArrayList<>();
+
+        classes.stream()
+                .filter(classNode -> classNode.superName.equals(owner.name))
+                .forEach(classNode -> {
+                    extending.add(classNode);
+                    extending.addAll(getExtending(classNode));
+                });
+
+        return extending;
+    }
+
+    protected void applyRemap(Map<String, String> remap) {
+        SimpleRemapper remapper = new SimpleRemapper(remap);
+
+        classes.forEach(clazz -> {
+            ClassNode copy = new ClassNode();
+            ClassRemapper adapter = new ClassRemapper(copy, remapper);
+
+            clazz.accept(adapter);
+            classMap.remove(clazz.name);
+            classMap.put(clazz.name, copy);
+        });
+    }
 }
