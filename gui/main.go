aa5d55c2d5f3cc6867df1c7654a6559357d79d7e
TerrificTable <omegagaming189@gmail.com>
2023-04-10 21:26:54 +0200

    initial 'gui'

--- gui/main.go
+++ gui/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var BRANCH, COMMIT string
+
+var (
+	version = "0.1.0"
+
+)
+
+func Version() {
+	fmt.Printf("Herbst GUI v%s (%s/%s)\n", version, BRANCH, COMMIT)
+}
+
 
 func main() {
-	fmt.Println("Hello World!")
+	Version()
 }
 
