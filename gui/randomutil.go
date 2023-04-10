ecf8a0d8be0d83862b9d457a9dc7c6b326a67ea6
TerrificTable <omegagaming189@gmail.com>
2023-04-10 22:42:21 +0200

    some utils moved outside main file since thats for the gui
    TODO: maybe move gui into its own file too

---
+++ gui/randomutil.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"math/rand"
+)
+
+
+var (
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+
+func RandomString(length int) string {
+	s := make([]rune, length)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(s)
+}
+
---
+++ gui/version.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+var (
+	version = "0.1.0"
+	BRANCH, COMMIT string
+)
+
+func Version() string {
+	return fmt.Sprintf("Herbst GUI v%s (%s/%s)\n", version, BRANCH, COMMIT)
+}
+
