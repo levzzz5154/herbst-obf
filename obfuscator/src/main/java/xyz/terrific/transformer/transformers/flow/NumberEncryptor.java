44b32bd6d7789fb15daf27f9ec9a6758ba5b496d
levzzz5154 <86072227+levzzz5154@users.noreply.github.com>
2023-06-08 11:16:21 +0500

    refactored NumberEncryptor

--- obfuscator/src/main/java/xyz/terrific/transformer/transformers/flow/NumberEncryptor.java
+++ obfuscator/src/main/java/xyz/terrific/transformer/transformers/flow/NumberEncryptor.java
@@ -5,6 +5,7 @@ import org.objectweb.asm.tree.*;
 import xyz.terrific.config.ConfigManager;
 import xyz.terrific.transformer.Transformer;
 import xyz.terrific.transformer.annotation.Group;
+import xyz.terrific.util.Logger;
 import xyz.terrific.util.RandomUtil;
 
 @Group(name = "flow")
@@ -13,73 +14,71 @@ public class NumberEncryptor extends Transformer {
     public void transform() {
         classes.stream().filter(classNode -> !isExcluded(classNode.name)).forEach(classNode -> {
             classNode.methods.forEach(methodNode -> {
-                for (int i = 0; i < 1; i++) {
-                    methodNode.instructions.forEach(insnNode -> {
-                        obfNumberConstant(methodNode, insnNode);
-                    });
+                final var insnArray = methodNode.instructions.toArray();
+                for (AbstractInsnNode insnNode : insnArray) {
+                    final var insnList = obfNumberConstant(insnNode);
+                    if (insnList.size() > 0) {
+                        Logger.getInstance().info(NumberEncryptor.class, "replaced " + insnList.size() + " number constants at " + classNode.name + "." + methodNode.name);
+                        methodNode.instructions.insert(insnNode, insnList);
+                        methodNode.instructions.remove(insnNode);
+                    }
                 }
             });
         });
     }
 
-    private static void obfNumberConstant(MethodNode methodNode, AbstractInsnNode insnNode) {
+    private static InsnList obfNumberConstant(final AbstractInsnNode insnNode) {
+        final var insnList = new InsnList();
         switch (insnNode) {
             case LdcInsnNode ldcInsn && ldcInsn.cst instanceof Integer -> {
                 final var key = RandomUtil.random.nextInt();
-                final var insnList = new InsnList();
+                var encryptedValue = ((int) ldcInsn.cst) ^ key;
 
-                ldcInsn.cst = ((int) ldcInsn.cst) ^ key;
                 if (RandomUtil.random.nextBoolean()) {
-                    ldcInsn.cst = Integer.reverse((int)ldcInsn.cst);
+                    encryptedValue = Integer.reverse((int)ldcInsn.cst);
+                    insnList.add(new LdcInsnNode(encryptedValue));
                     insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Integer", "reverse", "(I)I", false));
                 }
+                else insnList.add(new LdcInsnNode(encryptedValue));
+
                 insnList.add(new LdcInsnNode(key));
                 insnList.add(new InsnNode(Opcodes.IXOR));
-                methodNode.instructions.insert(insnNode, insnList);
             }
             case LdcInsnNode ldcInsn && ldcInsn.cst instanceof Long -> {
                 final var key = RandomUtil.random.nextLong();
-                ldcInsn.cst = ((long) ldcInsn.cst) ^ key;
-                methodNode.instructions.insertBefore(insnNode, new LdcInsnNode(key));
-                methodNode.instructions.insert(insnNode, new InsnNode(Opcodes.LXOR));
+                final var encryptedValue = ((long) ldcInsn.cst) ^ key;
+                insnList.add(new LdcInsnNode(key));
+                insnList.add(new LdcInsnNode(encryptedValue));
+                insnList.add(new InsnNode(Opcodes.LXOR));
             }
             case LdcInsnNode ldcInsn && ldcInsn.cst instanceof Float -> {
                 final var key = RandomUtil.random.nextInt();
                 final var value = (float)ldcInsn.cst;
-
-                final var insnList = new InsnList();
                 insnList.add(new LdcInsnNode(Float.floatToRawIntBits(value) ^ key));
                 insnList.add(new LdcInsnNode(key));
                 insnList.add(new InsnNode(Opcodes.IXOR));
                 insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Float", "intBitsToFloat", "(I)F", false));
-
-                methodNode.instructions.insert(insnNode, insnList);
-                methodNode.instructions.remove(insnNode);
             }
             case LdcInsnNode ldcInsn && ldcInsn.cst instanceof Double -> {
                 final var key = RandomUtil.random.nextLong();
                 final var value = (double)ldcInsn.cst;
 
-                final var insnList = new InsnList();
                 insnList.add(new LdcInsnNode(Double.doubleToRawLongBits(value) ^ key));
                 insnList.add(new LdcInsnNode(key));
                 insnList.add(new InsnNode(Opcodes.LXOR));
                 insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Double", "longBitsToDouble", "(J)D", false));
-
-                methodNode.instructions.insert(insnNode, insnList);
-                methodNode.instructions.remove(insnNode);
             }
             case IntInsnNode intInsn && (intInsn.getOpcode() == Opcodes.SIPUSH || intInsn.getOpcode() == Opcodes.BIPUSH) -> {
                 final var key = RandomUtil.random.nextInt();
                 final var value = intInsn.operand;
 
-                methodNode.instructions.insertBefore(insnNode, new LdcInsnNode(key));
-                methodNode.instructions.insert(insnNode, new InsnNode(Opcodes.IXOR));
-                methodNode.instructions.insert(insnNode, new LdcInsnNode(value ^ key));
-                methodNode.instructions.remove(insnNode);
+                insnList.add(new LdcInsnNode(key));
+                insnList.add(new LdcInsnNode(value ^ key));
+                insnList.add(new InsnNode(Opcodes.IXOR));
             }
             default -> {}
         }
+        return insnList;
     }
 
     @Override
